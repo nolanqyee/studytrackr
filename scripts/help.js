607037967f5562fbd77ab2d3d7162f779cfecc5d
nolanqyee <nolanyee99@gmail.com>
2022-11-23 21:38:47 -0600

    s

--- scripts/help.js
+++ scripts/help.js
@@ -1,14 +1,10 @@
 var btn = document.getElementById("helpbtn");
 var modal = document.getElementById("helpModal");
-var span = document.getElementsByClassName("helpclose")[0];
 //open
 btn.onclick = function() {
   modal.style.display = "block";
 }
-//close
-span.onclick = function() {
-  modal.style.display = "none";
-}
+
 //close if click outside
 window.onclick = function(event) {
   if (event.target == modal) {
